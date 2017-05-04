32d0231c30b537e71ca9e0ddfb6801b4ca17e803
kevin <dekeyser.kevin97@gmail.com>
2017-05-04 14:00:45 +0200

    Add genLevelNames

--- genLevelNames/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-int main() {
-	int start,end;
-	cin >> start >> end;
-	for(int i = start; i <= end; ++i) cout << "levels.push_back(l"<< i <<");" << endl;
-	return 0;
-}
