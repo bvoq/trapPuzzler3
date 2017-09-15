7019bcaf4c38a78804c46990640dc3224aa6b84b
kevin <dekeyser.kevin97@gmail.com>
2017-09-15 17:51:53 +0200

    Level settings for 10x10

--- src/levelGen.h
+++ src/levelGen.h
@@ -19,11 +19,11 @@ deque<deque<int> > initField(int h, int w) {
     vector<vector<pair<int,int> > > stonesYellow = stonesBlack;
     int minPlayers = 2, maxPlayers = 2;
     int yellowBlocksToBePlaced = minPlayers+(rand()%(maxPlayers-minPlayers+1));
-    int darkBlocksToBePlaced = 7 + (rand()%8);
-    int redBlocksToBePlaced = 2 + (rand()%14);
+    //int darkBlocksToBePlaced = 7 + (rand()%8);
+    //int redBlocksToBePlaced = 2 + (rand()%14);
     //IDEAL FOR 10x10
-    //int darkBlocksToBePlaced = 15 + (rand() % 15);
-    //int redBlocksToBePlaced = 10 + (rand()%10);
+    int darkBlocksToBePlaced = 15 + (rand() % 15);
+    int redBlocksToBePlaced = 10 + (rand()%10);
     
     while(yellowBlocksToBePlaced != 0) {
         //offsetY = h/2; offsetX = w/2;
