c50194ec4c590867968b8391c25639b0e6f66ec9
kevin <dekeyser.kevin97@gmail.com>
2017-09-17 12:10:56 +0200

    Slurp voices raw

--- src/audio.h
+++ src/audio.h
@@ -3,12 +3,14 @@
 
 
 vector<ofSoundPlayer> titles, walksfx, undosfx, blockingsfx;
+float musicvolume = 0.75, sfxvolume = 1.0;
 int currentTitle = -1;
 void initAudio() {
     for(int i = 0; i <= 0; ++i) {
         ofSoundPlayer temp;
         temp.load(locationOfResources + "audio/title"+to_string(i)+".mp3");
         temp.setLoop(true);
+        temp.setVolume(musicvolume);
         titles.push_back(temp);
     }
     
@@ -16,6 +18,7 @@ void initAudio() {
         ofSoundPlayer temp;
         temp.load(locationOfResources + "audio/walk"+to_string(i)+".wav");
         temp.setLoop(false);
+        temp.setVolume(sfxvolume);
         walksfx.push_back(temp);
     }
     
@@ -23,6 +26,7 @@ void initAudio() {
         ofSoundPlayer temp;
         temp.load(locationOfResources + "audio/undo"+to_string(i)+".wav");
         temp.setLoop(false);
+        temp.setVolume(sfxvolume);
         undosfx.push_back(temp);
     }
     
@@ -30,15 +34,16 @@ void initAudio() {
         ofSoundPlayer temp;
         temp.load(locationOfResources + "audio/blocking"+to_string(i)+".wav");
         temp.setLoop(false);
+        temp.setVolume(sfxvolume);
         blockingsfx.push_back(temp);
     }
 }
 
 void playAudio(int title) {
     if(currentTitle != title) {
-        //if(currentTitle != -1) titles[title].stop();
-        //titles[title].play();
-        //currentTitle = title;
+        if(currentTitle != -1) titles[title].stop();
+        titles[title].play();
+        currentTitle = title;
     }
 }
 
@@ -58,5 +63,9 @@ void playBlocking() {
     blockingsfx[pos].play();
 }
 
+
+void playGravity() {
+    
+}
 
 #endif /* audio_h */
