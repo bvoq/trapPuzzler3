9ee98687e6809750020c3c5ea77087db5a4165da
kevin <dekeyser.kevin97@gmail.com>
2017-10-01 23:02:30 +0200

    blub

--- bin/winchange.sh
+++ bin/winchange.sh
@@ -5,6 +5,7 @@ mkdir ｔｒａｐｐｕｚｚｌｅｒwin
 cp -a data ｔｒａｐｐｕｚｚｌｅｒwin/data
 cp READMEWIN.txt ｔｒａｐｐｕｚｚｌｅｒwin/README.txt
 cp trapPuzzler3.exe ｔｒａｐｐｕｚｚｌｅｒwin/ｔｒａｐｐｕｚｚｌｅｒ.exe 
+cp *.dll ｔｒａｐｐｕｚｚｌｅｒwin/
 zip -r ｔｒａｐｐｕｚｚｌｅｒwin.zip ｔｒａｐｐｕｚｚｌｅｒwin
 rm -rf ｔｒａｐｐｕｚｚｌｅｒwin
 
