386273b9fc1f5e0cde41ce5729d008f8d8fabc59
kevin <dekeyser.kevin97@gmail.com>
2017-10-01 22:58:23 +0200

    Added compile scripts

--- bin/winchange.sh
+++ bin/winchange.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-rm -rf ｔｒａｐｐｕｚｚｌｅｒwin 
+rm -rf ｔｒａｐｐｕｚｚｌｅｒwin
 mkdir ｔｒａｐｐｕｚｚｌｅｒwin
 cp -a data ｔｒａｐｐｕｚｚｌｅｒwin/data
 cp READMEWIN.txt ｔｒａｐｐｕｚｚｌｅｒwin/README.txt
-mv trapPuzzler3.exe ｔｒａｐｐｕｚｚｌｅｒwin/ｔｒａｐｐｕｚｚｌｅｒ.exe 
+cp trapPuzzler3.exe ｔｒａｐｐｕｚｚｌｅｒwin/ｔｒａｐｐｕｚｚｌｅｒ.exe 
+zip -r ｔｒａｐｐｕｚｚｌｅｒwin.zip ｔｒａｐｐｕｚｚｌｅｒwin
+rm -rf ｔｒａｐｐｕｚｚｌｅｒwin
 
